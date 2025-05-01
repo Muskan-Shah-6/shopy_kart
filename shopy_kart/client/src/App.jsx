ffcd7b0afd5385d372dc46457425cedd88aba3cc
muskiofficially <shahmadarmuskan48@gmail.com>
2025-05-01 18:50:56 +0530

    Muskan
    Done

---
+++ shopy_kart/client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react' 
+import './App.css'
+
+function App() { 
+
+  return (
+    <>
+      <h1 className='text-2xl text-red-500 text-center underline mt-10 animate-bounce'>Testingt the react app and also the working of tailwind css</h1>
+    </>
+  )
+}
+
+export default App
