8270deea1244d470a76553c2de0c544cb57adbec
muskiofficially <shahmadarmuskan48@gmail.com>
2025-05-25 19:37:14 +0530

    Home
    frontend side work started, navigation bar added

--- shopy_kart/client/src/App.jsx
+++ shopy_kart/client/src/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react' 
 import './App.css'
+import {Outlet} from 'react-router-dom'
+import Navigation from './pages/Auth/Navigation'
+import {ToastContainer} from 'react-toastify' 
+import '../node_modules/react-toastify/dist/ReactToastify.css'
+
 
 function App() { 
 
   return (
     <>
-      <h1 className='text-2xl text-red-500 text-center underline mt-10 animate-bounce'>Testingt the react app and also the working of tailwind css</h1>
+      <ToastContainer/>
+        <Navigation />
+        <main className='py-3'>
+          <Outlet />
+        </main>
+      
     </>
   )
 }
---
+++ shopy_kart/client/src/redux/constants.js
@@ -0,0 +1,2 @@
+export const BASE_URL = '' //used as proxy
+export const USERS_URL = '/api/users'
